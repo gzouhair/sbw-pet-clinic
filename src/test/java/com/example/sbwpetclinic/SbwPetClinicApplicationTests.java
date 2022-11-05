7c15bd9977f5daec9db2c49d01b15360856e8e57
zgabouri <zouhairgabouri@gmail.com>
2022-11-05 19:17:36 +0100

    Intial Spring boot Project

---
+++ src/test/java/com/example/sbwpetclinic/SbwPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbwpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbwPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
